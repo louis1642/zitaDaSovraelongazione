02f8b69b8f50668be6c15111737d2f90dfb2f913
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2021-11-08 16:22:38 +0100

    Robustezza funzione

--- zitaDaSovraelongazione.m
+++ zitaDaSovraelongazione.m
@@ -3,6 +3,11 @@ function z = zitaDaSovraelongazione(s_perc)
 %ottenere la sovraelongazione percentuale richiesta
 %   Il valore di zita viene calcolato utilizzando la funzione inversa della
 %   relazione s% = 100 exp(-zita*pi/sqrt(1-zita^2))
+%   
+%   s% deve essere compreso tra 0 e 100
+arguments
+    s_perc (1,1) {mustBeNumeric,mustBeGreaterThanOrEqual(s_perc,0),mustBeLessThanOrEqual(s_perc,100)}
+end
 if (s_perc == 0)
     z = 1;
 else
